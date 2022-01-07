b6ccac24b2621db3400523d08ecd65337d90a426
Sam <spirited.scholar10@gmail.com>
2022-01-07 08:31:45 -0700

    first

---
+++ Matching Game/Matching Game/NavView.swift
@@ -0,0 +1,20 @@
+//
+//  NavView.swift
+//  Matching Game
+//
+//  Created by Sam Hiatt  on 1/7/22.
+//
+
+import SwiftUI
+
+struct NavView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavView()
+    }
+}
