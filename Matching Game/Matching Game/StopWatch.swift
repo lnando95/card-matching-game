bc7e18174f0d5316a367c2ac6a20b0ba85a7559d
Sam <spirited.scholar10@gmail.com>
2022-01-13 17:10:57 -0700

    StopWatch Commit

---
+++ Matching Game/Matching Game/StopWatch.swift
@@ -0,0 +1,49 @@
+import SwiftUI
+
+class StopWatchManager: ObservableObject{
+
+    @Published var secondsElapsed = 0.0
+
+    var startTime: Date = Date()
+
+    var timer = Timer()
+
+    func startWatch(){
+        startTime = Date()
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true){ timer in
+            let current = Date()
+            let diffComponents = Calendar.current.dateComponents([.second, .nanosecond], from: self.startTime, to: current)
+            let seconds = Double(diffComponents.second ?? 0) + Double(diffComponents.nanosecond ?? 0) / 1_000_000_000
+            self.secondsElapsed += seconds
+            self.startTime = current
+        }
+
+    }
+
+    func pauseWatch(){
+        timer.invalidate()
+    }
+}
+
+struct StopWatchView: View {
+
+    @ObservedObject var stopWatchManager = StopWatchManager()
+
+    var body: some View{
+        HStack{
+            Button("Start"){
+                stopWatchManager.startWatch()
+            }
+            Text("\(stopWatchManager.secondsElapsed, specifier: "%.0f")")
+            Button("Pause"){
+                stopWatchManager.pauseWatch()
+            }
+        }
+    }
+}
+
+struct StopWatchView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
