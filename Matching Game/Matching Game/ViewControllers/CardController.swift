af22ac7aee9295bb0c2eaf3123a3c6e644e46090
Sam <spirited.scholar10@gmail.com>
2022-02-15 08:49:10 -0700

    Big Commit

--- Matching Game/Matching Game/ViewControllers/CardController.swift
+++ Matching Game/Matching Game/ViewControllers/CardController.swift
@@ -3,48 +3,54 @@ import SwiftUI
 // step 1. remove isFlip from card
 // step 2. instead of isFlip reference cardGame.matches and cardGame firstFaceupCard and secondFaceUpCard
 class CardGame: ObservableObject {
-  var cards: [Card]
-    var gameHasEnded: Bool = false
-  @Published var matches: [Card] = []
-  @Published var firstFaceUpCard: Card?
-  @Published var secondFaceUpCard: Card?
-  @Published var numberOfGuesses = 0
-  func flipCard(card: Card) {
-    numberOfGuesses += 1
-    // this function will handle flipping the card and unflipping after a second
-    if firstFaceUpCard == nil {
-      firstFaceUpCard = card
-    } else {
-      secondFaceUpCard = card
-      DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
-        self.updateMatches()
+    var cards: [Card]
+      @Published var matches: [Card] = []
+      @Published var firstFaceUpCard: Card?
+      @Published var secondFaceUpCard: Card?
+      @Published var numberOfGuesses = 0
+      func flipCard(card: Card) {
+          numberOfGuesses += 1
+          if firstFaceUpCard != nil && secondFaceUpCard != nil {
+              // this is the third card tapped, unlflips the first two cards.
+              firstFaceUpCard = nil
+              secondFaceUpCard = nil
+          }
+          // this function will handle flipping the card and unflipping after a second
+          if firstFaceUpCard == nil {
+              firstFaceUpCard = card
+          } else {
+              secondFaceUpCard = card
+              DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                  self.updateMatches()
+              }
+          }
       }
-    }
-  }
-  func cardIsFlipped(card: Card) -> Bool {
-    return matches.contains { currentCard in
-      card.id == currentCard.id
-    } || firstFaceUpCard?.id == card.id || secondFaceUpCard?.id == card.id
-  }
-  func updateMatches() {
-    guard let firstFaceUpCard = firstFaceUpCard, let secondFaceUpCard = secondFaceUpCard else {return}
-    // this is the second card guess, verify if its a match or not
-    if firstFaceUpCard.image == secondFaceUpCard.image {
-      // it's a match
-      matches.append(firstFaceUpCard)
-      matches.append(secondFaceUpCard)
-    }
-    // if it's not a match, do nothing
-    self.firstFaceUpCard = nil
-    self.secondFaceUpCard = nil
-  }
-  init(numberOfPairsofCards: Int, createdContent: (Int) -> String) {
-    cards = []
-    for pairIndex in 0..<numberOfPairsofCards {
-      let image = createdContent(pairIndex)
-      cards.append(Card(image: image, id: pairIndex * 2 ))
-      cards.append(Card(image: image, id: pairIndex * 2 + 1))
-    }
-    cards.shuffle()
+          func cardIsFlipped(card: Card) -> Bool {
+                return matches.contains { currentCard in
+                    card.id == currentCard.id
+                } || firstFaceUpCard?.id == card.id || secondFaceUpCard?.id == card.id
+            }
+        
+        func updateMatches() {
+               guard let firstFaceUpCard = firstFaceUpCard, let secondFaceUpCard = secondFaceUpCard else {return}
+               // this is the second card guess, verify if its a match or not
+               if firstFaceUpCard.image == secondFaceUpCard.image {
+                   // it's a match
+                   matches.append(firstFaceUpCard)
+                   matches.append(secondFaceUpCard)
+               }
+               //    // if it's not a match, do nothing
+               //    self.firstFaceUpCard = nil
+               //    self.secondFaceUpCard = nil
+           }
+           init(numberOfPairsofCards: Int, createdContent: (Int) -> String) {
+               cards = []
+               for pairIndex in 0..<numberOfPairsofCards {
+                   let image = createdContent(pairIndex)
+                   cards.append(Card(image: image, id: pairIndex * 2 ))
+                   cards.append(Card(image: image, id: pairIndex * 2 + 1))
+               }
+               cards.shuffle()
+
   }
 }
--- Matching Game/Matching Game/Views/CardView.swift
+++ Matching Game/Matching Game/Views/CardView.swift
@@ -39,32 +39,18 @@ struct CardView: View {
         .onReceive(memoryGame.model.objectWillChange) { _ in
             flipCard()
         }
-    
- struct CardView: View {
-  var card: Card
-  @ObservedObject var memoryGame: MemoryGame
-     @State private var degrees = 0.0
-  var body: some View {
-    ZStack {
-  let rectangle = RoundedRectangle(cornerRadius: 20.0)
-    if memoryGame.model.cardIsFlipped(card: card) {
-      rectangle.fill(.white)
-        Text(card.image)
-        .font(Font.largeTitle)
-    } else {
-      rectangle.fill(.blue)
 
-    }
-    func flipCard() {
-        if memoryGame.model.cardIsFlipped(card: card) {
-            animationAmount = -180
-            fillColor = .white
-        } else {
-            animationAmount = 0
-            fillColor = .blue
-        }
-    }
-    
+}
+      func flipCard() {
+             if memoryGame.model.cardIsFlipped(card: card) {
+                 animationAmount = -180
+                 fillColor = .white
+             } else {
+                 animationAmount = 0
+                 fillColor = .blue
+             }
+         }
+
     struct CardView_Previews: PreviewProvider {
         static var previews: some View {
             CardView(card: Card(image: ":expressionless:", id: 23), memoryGame: MemoryGame(gameOption: GameOption.emojiOption))
